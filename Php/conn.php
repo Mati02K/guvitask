691451b3fe2105631f1cfaa3f520eb3fc1f69c11
Mati02K <mati02official@gmail.com>
2021-10-03 02:06:22 +0530

    Remote Connection Added

--- Php/conn.php
+++ Php/conn.php
@@ -1,9 +1,16 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "guvi";
+// Developement Environment
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "guvi";
+
+// Connection credentials
+$servername = "remotemysql.com";
+$username = "zzmlknS7k2";
+$password = "iQjKgG0s3H";
+$dbname = "zzmlknS7k2";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
